b598fa1be7e79af11c7e321bb589f6d572038bf5
Thorsten Schwachhofer <thorsten.schwachhofer@fau.de>
2019-11-28 16:12:04 +0100

    move index.ts into src

--- index.ts
+++
@@ -1,7 +0,0 @@
-import {Promise} from 'es6-shim';
-
-export {WebWorkerTransport} from './src/Transport/WebWorkerTransport';
-export {WebSocketTransport} from './src/Transport/WebSocketTransport';
-export {Client} from './src/Client';
-export {retryWhen} from './src/Client';
-export * from './src/Client';
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {Promise} from 'es6-shim';
+
+export {WebWorkerTransport} from './Transport/WebWorkerTransport';
+export {WebSocketTransport} from './Transport/WebSocketTransport';
+export {Client} from './Client';
+export {retryWhen} from './Client';
+export * from './Client';
